768d9de83d5c9e35429b8280e60663910d0d1d3c
Jake Hilton <jakerscholes@gmail.com>
2020-12-05 16:03:07 -0700

    adding colors to rows

--- Develop/logic.js
+++ Develop/logic.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    $("#currentDay").text(moment().format("llll"));
     $(".btn").click(function() {
     var description = $(this).siblings(".description").val();
     var rowId = $(this).parent().attr("id");
@@ -7,7 +8,29 @@ $(document).ready(function() {
     console.log(description);
     });
 
-    
+    function rowColors() {
+        var now = moment().hours();
+        console.log(now);
+
+        $(".time-block").each(function() {
+            var getTime = parseInt($(this).attr("id").split("-")[1]);
+            console.log(typeof(getTime));
+
+            if (now < getTime) {
+                $(this).addClass("past");
+                console.log("Correct");
+            } else if (getTime === now) {
+                $(this).removeClass("past")
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+            setInterval(rowColors, 1000);
+        })
+    }
+    rowColors();
 })
 
 
